d9504e8786c2e900d2f2f79324a28ee1a9ff3568
Hirra Sheikh <hirra.sheikh@hotmail.com>
2015-10-01 12:58:31 -0400

    Changed names

--- exercise1.py
+++ exercise1.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 
-""" Assignment 1, Exercise 1, INF1340, Fall, 2014. Grade to gpa conversion
+""" Assignment 1, Exercise 1, INF1340, Fall, 2015. Grade to gpa conversion
 
 This module prints the amount of money that Lakshmi has remaining
 after the stock transactions
 
 """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
-__copyright__ = "2015 Susan Sim"
-__license__ = "MIT License"
+__author__ = 'Hirra and Tyler'
 
-
-money = 20000.00
-print(money)
