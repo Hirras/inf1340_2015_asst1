591a17ada0d114d24fbc9a17d4444d42faf0414b
Hirra Sheikh <hirra.sheikh@hotmail.com>
2015-10-01 14:05:20 -0400

    completed exercise 3

--- exercise3.py
+++ exercise3.py
@@ -14,19 +14,42 @@ __license__ = "MIT License"
 
 
 def diagnose_car():
+
     """
     Interactively queries the user with yes/no questions to identify a
     possible issue with a car.
 
-    Inputs:
+    Inputs: answer
 
-    Expected Outputs:
+    Expected Outputs: diagnostic
 
     Errors:
 
     """
-
-    print("The battery cables may be damaged. Replace cables and try again.")
-
+    answer = raw_input("Is the car silent when you turn the key? ")
+    if answer == "y":
+        answer = raw_input("Are the battery terminals corroded? ")
+        if answer == "y":
+            print ("Clean terminals and try starting again")
+        elif answer == "n":
+            print("The battery cables may be damaged. Replace cables and try again.")
+    elif answer == "n":
+        answer = raw_input("Does the car make a clicking noise? ")
+        if answer == "y":
+             print ("Replace the battery.")
+        elif answer == "n":
+            answer = raw_input("Does the car crank up but fail to start? ")
+            if answer == "y":
+                print ("Check spark plug connections.")
+            elif answer == "n":
+                answer = raw_input("Does the engine start and then die? ")
+                if answer == "y":
+                    answer = raw_input("Does the car have fuel injection? ")
+                    if answer == "y":
+                        print ("Get it in for service.")
+                    elif answer == "n":
+                        print ("Check to ensure the choke is opening and closing.")
+                elif answer == "n":
+                    print ("Your car is functioning.")
 
 diagnose_car()
